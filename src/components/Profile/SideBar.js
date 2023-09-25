a0ed73697fc42d967724f9010ca39fe9a9971759
martytrajkoski1 <marti.trajkoski@students.finki.ukim.mk>
2023-09-25 13:41:25 +0200

    router, profile-implementation

---
+++ src/components/Profile/SideBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
+
+export default function SideBar() {
+    return(
+        <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
+        <CDBSidebar textColor="#fff" >
+          <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>} style={{backgroundColor: "#3C327B"}}>
+            <p>Profile</p>
+          </CDBSidebarHeader>
+  
+          <CDBSidebarContent className="sidebar-content" style={{backgroundColor: "#554f7a"}}>
+            <CDBSidebarMenu>
+              <NavLink exact to="/" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="user">User Info</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/tables" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="credit-card">Subscription</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/profile" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="table">Courses</CDBSidebarMenuItem>
+              </NavLink>
+            </CDBSidebarMenu>
+          </CDBSidebarContent>
+  
+          <CDBSidebarFooter style={{ textAlign: 'center' }}>
+            <div
+              style={{
+                padding: '20px 5px',
+              }}
+            >
+              Sidebar Footer
+            </div>
+          </CDBSidebarFooter>
+        </CDBSidebar>
+      </div>
+    );
+   
+};
---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import Navibar from "../components/Navbar/Navibar";
+import SideBar from "../components/Profile/SideBar";
+
+export default function Profile(){
+    return(
+        <div>
+            <Navibar></Navibar>
+            <SideBar></SideBar>
+        </div> 
+    );
+}

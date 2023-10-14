78091aecccbe97c981953b584dfd94a39d5d90f3
martytrajkoski <martytrajkoski@gmail.com>
2023-10-15 00:48:23 +0200

    djangoAPI Successful

--- src/pages/Photoshop.js
+++ src/pages/Photoshop.js
@@ -1,11 +1,26 @@
+import React from "react";
+import axios from "axios";
 import Navibar from "../components/Navbar/Navibar";
 import SideBar from "../components/Photoshop/SideBar";
+import { Button } from "react-bootstrap";
 
 export default function Photoshop(){
+
+    const getTutorial = () => {
+        axios.get('http://127.0.0.1:8000/api/tutorials/')
+        .then(res => {
+            console.log(res.data)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
     return(
         <div>
             <Navibar ></Navibar>
-            <SideBar/>
+            <Button onClick={getTutorial}>tutorial</Button>
+            {/* <SideBar/> */}
+            {/* {tutorial.name} */}
         </div>
     );
 }
